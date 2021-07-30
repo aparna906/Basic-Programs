92cce649234c29c91f83927b63cca75d9bfce9e7
Aparna Misar <aparnamisar28@gmail.com>
2021-07-30 12:59:06 +0530

    init

---
+++ sumOfArrayElements/Logic1.java
@@ -0,0 +1,10 @@
+class Logic1 {
+ public static void main(String[] args) {
+     int A[] = {1,2,10,4,5};
+     int sum = 0;
+     for(int i=0;i<A.length;i++){
+         sum = sum + A[i];
+     }
+     System.out.println(sum);
+ }    
+}
---
+++ sumOfArrayElements/Logic2.java
@@ -0,0 +1,10 @@
+class Logic2 {
+ public static void main(String[] args) {
+     int A[] = {2,3,4,6,8};
+     int sum = 0;
+     for(int value:A){
+        sum = sum + value;
+     }
+     System.out.println("Sum of Array Element : "+sum);
+ }    
+}
