c7e37fb9da64d5659ff48f9dfb70c82137267ad6
Aparna Misar <aparnamisar28@gmail.com>
2021-08-03 19:58:11 +0530

    init

---
+++ removeWhiteSpaces/Logic.java
@@ -0,0 +1,7 @@
+class Logic {
+    public static void main(String[] args) {
+        String s = "Hello    Everyone  ";
+        String s1 = s.replaceAll("\\s", "");
+        System.out.println(s1);
+    }
+}
